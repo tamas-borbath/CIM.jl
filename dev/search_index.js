f161cdfb70a2d3e75fcb65dfd64cc6d7a79c8b56
Documenter.jl <documenter@juliadocs.github.io>
2020-11-15 15:16:47 +0000

    build based on fde7ef8

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = CIM","category":"page"},{"location":"#CIM","page":"Home","title":"CIM","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"This package is not functional, do not use it.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [CIM]","category":"page"}]
+}
